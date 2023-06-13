07ce288162dcad22ae151b3715fd68daea164ae5
daddybear <sihlebarrett@gmail.com>
2023-06-13 10:29:48 +0200

    Update refact.js

--- refact.js
+++ refact.js
@@ -1,128 +1,85 @@
-import { books, authors, genres, BOOKS_PER_PAGE } from "./data.js";
-// initial page and book matches
-let page = 1;
-let matches = books;
+import { BOOKS_PER_PAGE, authors, genres, books } from './data.js'
 
-// Generate and append preview elements for the initial page
-const starting = generatePreviewElements(matches.slice(0, BOOKS_PER_PAGE));
-document.querySelector("[data-list-items]").appendChild(starting);
-
-// Generate and append genre options to search dropdown
-const genreOptions = generateGenreOptions(genres);
-document.querySelector("[data-search-genres]").appendChild(genreOptions);
-
-// Generate and append author options to search dropdown
-const authorOptions = generateAuthorOptions(authors);
-document.querySelector("[data-search-authors]").appendChild(authorOptions);
-
-// Set page theme based on user preference or default
-setTheme();
-updateListButtonMessage(matches.length);
-
-/**
- * Set page theme based on user preference or default
- */
-setTheme = () => {
-  if (
-    window.matchMedia &&
-    window.matchMedia("(prefers-color-scheme: dark)").matches
-  ) {
-    document.querySelector("[data-settings-theme]").value = "night";
-    document.documentElement.style.setProperty("--color-dark", "255, 255, 255");
-    document.documentElement.style.setProperty("--color-light", "10, 10, 20");
-  } else {
-    document.querySelector("[data-settings-theme]").value = "day";
-    document.documentElement.style.setProperty("--color-dark", "10, 10, 20");
-    document.documentElement.style.setProperty(
-      "--color-light",
-      "255, 255, 255"
-    );
-  }
-};
-/**
- * Generate a document fragment of preview elements
- * @param {Array} books - Array of book objects
- * @returns {DocumentFragment} - Document fragment containing
- *  preview elements
- */
-generatePreviewElements = (books) => {
-  const fragment = document.createDocumentFragment();
-  for (const { author, id, image, title } of books) {
-    const element = generatePreviewElement({ author, id, image, title });
-    fragment.appendChild(element);
-  }
-  return fragment;
-};
-
-/**
- * Generate a single preview element
- * @param {Object} book - Book object containing author, id, image, and title properties
- * @returns {HTMLButtonElement} - Button element with preview HTML content
- */
-generatePreviewElement = ({ author, id, image, title }) => {
-  const element = document.createElement("button");
-  element.classList = "preview";
-  element.setAttribute("data-preview", id);
-  // preview element's HTML content
-  element.innerHTML = `<img class="preview__image" src="${image}" />
-    <div class="preview__info">
-      <h3 class="preview__title">${title}</h3>
-      <div class="preview__author">${authors[author]}</div>
-    </div>`;
-
-  return element;
-};
-
-/**
- * Generate a document fragment of genre options
- * @param {Object} genres - Object containing genre IDs and names
- * @returns {DocumentFragment} - Document fragment containing genre option elements
- */
-generateGenreOptions = (genres) => {
-  const fragment = document.createDocumentFragment();
-
-  // Add a default 'All Genres' option
-  const firstGenreElement = generateGenreOption("any", "All Genres");
-  fragment.appendChild(firstGenreElement);
-
-  // Add an option for each genre in the genres object
-  for (const [id, name] of Object.entries(genres)) {
-    fragment.appendChild(generateGenreOption(id, name));
+class BookList {
+  constructor(books) {
+    this.books = books
+    this.page = 1
+    this.matches = books
   }
 
-  return fragment;
-};
+  getResults(filters) {
+    const result = []
 
-generateGenreOption = (id, name) => {
-  const element = document.createElement("option");
-  element.value = id;
-  element.innerText = name;
-  return element;
-};
+    for (const book of this.books) {
+      let genreMatch = filters.genre === 'any'
 
-generateAuthorOptions = (authors) => {
-  const fragment = document.createDocumentFragment();
-  const firstAuthorElement = generateAuthorOption("any", "All Authors");
-  fragment.appendChild(firstAuthorElement);
+      for (const singleGenre of book.genres) {
+        if (genreMatch) break;
+        if (singleGenre === filters.genre) { genreMatch = true }
+      }
 
-  for (const [id, name] of Object.entries(authors)) {
-    fragment.appendChild(generateAuthorOption(id, name));
+      if (
+        (filters.title.trim() === '' || book.title.toLowerCase().includes(filters.title.toLowerCase())) &&
+        (filters.author === 'any' || book.author === filters.author) &&
+        genreMatch
+      ) {
+        result.push(book)
+      }
+    }
+
+    return result
   }
 
-  return fragment;
-};
+  render() {
+    const fragment = document.createDocumentFragment()
 
-generateAuthorOption = (id, name) => {
-  const element = document.createElement("option");
-  element.value = id;
-  element.innerText = name;
-  return element;
-};
+    for (const { author, id, image, title } of this.matches.slice(this.page * BOOKS_PER_PAGE, (this.page + 1) * BOOKS_PER_PAGE)) {
+      const element = document.createElement('button')
+      element.classList = 'preview'
+      element.setAttribute('data-preview', id)
 
-updateListButtonMessage = (count) => {
-  document.querySelector(
-    "[data-list-button]"
-  ).innerHTML = `<span>Show more</span> <span class="list__remaining">${
-    count > 0 ? count : 0
-  }</span>`;
-};
+      element.innerHTML = `
+        <img
+          class="preview__image"
+          src="${image}"
+        />
+
+        <div class="preview__info">
+          <h3 class="preview__title">${title}</h3>
+          <div class="preview__author">${authors[author]}</div>
+        </div>
+      `
+
+      fragment.appendChild(element)
+    }
+
+    document.querySelector('[data-list-items]').appendChild(fragment)
+  }
+
+  showMore() {
+    if (this.matches.length > (this.page + 1) * BOOKS_PER_PAGE) {
+      this.page += 1
+      this.render()
+    }
+  }
+
+  onPreviewClick(event) {
+    const preview = event.target.closest('.preview')
+    const book = this.books.find(book => book.id === preview.dataset.preview)
+
+    if (book) {
+      document.querySelector('[data-list-active]').open = true
+      document.querySelector('[data-list-blur]').src = book.image
+      document.querySelector('[data-list-image]').src = book.image
+      document.querySelector('[data-list-title]').innerText = book.title
+      document.querySelector('[data-list-subtitle]').innerText = `${authors[book.author]} (${new Date(book.published).getFullYear()})`
+      document.querySelector('[data-list-description]').innerText = book.description
+    }
+  }
+}
+
+const bookList = new BookList(books)
+
+document.querySelector('[data-list-items]').addEventListener('click', bookList.onPreviewClick)
+
+document.querySelector('[data-list-button]').addEventListener('click', bookList.showMore)
